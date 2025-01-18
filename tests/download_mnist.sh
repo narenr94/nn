3a684617b15ae75beeec188edc3973006ab3c96c
narenr94 <narenr94@gmail.com>
2025-01-18 14:30:55 +0530

    Bug Fix 0004, tests cmakelists download mnist data once

--- tests/download_mnist.sh
+++ tests/download_mnist.sh
@@ -1,10 +1,12 @@
-if [ ! -d "../build/examples/MNIST" ]; then
+if [ ! -d "./MNIST" ]; then
   cd ../build/examples/
   mkdir ./MNIST
   cd ./MNIST
   wget "https://pjreddie.com/media/files/mnist_train.csv"
   wget "https://pjreddie.com/media/files/mnist_test.csv"
   cd ../
+else
+  echo "MNIST directory already exists!!!"
 fi
 
 
